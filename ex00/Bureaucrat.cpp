a0e2a27a8a15638c02e3afb826a68478fc0b0b19
tmurua <tommurua@gmail.com>
2026-04-27 11:10:55 +0200

    feat: add Bureaucrat class and empty main. TODO: main with try and catch tests

---
+++ ex00/Bureaucrat.cpp
@@ -0,0 +1,66 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Bureaucrat.cpp                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tmurua <tmurua@student.42berlin.de>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/27 09:56:11 by tmurua            #+#    #+#             */
+/*   Updated: 2026/04/27 11:08:22 by tmurua           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Bureaucrat.hpp"
+#include <string>
+
+Bureaucrat::Bureaucrat() : _name("Mr. Nobody"), _grade(150){}
+
+Bureaucrat::Bureaucrat(const std::string &name,
+					   int grade) : _name(name), _grade(grade){
+	if (_grade < 1)
+		throw GradeTooHighException();
+	if (_grade > 150)
+		throw GradeTooLowException();
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat &other) : _name(other._name), 
+						_grade(other._grade){}
+
+// partial assignment, only assigns _grade, cuz _name is const
+Bureaucrat	&Bureaucrat::operator=(const Bureaucrat &other){
+	if (this != &other)
+		_grade = other._grade; // or _grade = other.getGrade(), what's better practice?
+	return (*this);
+}
+
+Bureaucrat::~Bureaucrat(){}
+
+
+const std::string	&Bureaucrat::getName() const{
+	return (_name);
+}
+
+int					Bureaucrat::getGrade() const{
+	return (_grade);
+}
+
+
+void	Bureaucrat::incrementGrade(){
+if (_grade <= 1)
+		throw GradeTooHighException();
+	_grade--;
+}
+
+void	Bureaucrat::decrementGrade(){
+	if (_grade >= 150)
+		throw GradeTooLowException();
+	_grade++;
+}
+
+const char* Bureaucrat::GradeTooHighException::what() const throw(){
+	return ("This grade is way too high! NOOO!");
+}
+
+const char* Bureaucrat::GradeTooLowException::what() const throw(){
+	return ("This grade is too low... We can't do that :/");
+}
